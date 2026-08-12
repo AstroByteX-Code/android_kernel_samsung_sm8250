2457bfd0e114aa8b87f2c885eeeb490266b3477b
Mian Hamza <codebymianhamza@gmail.com>
2026-08-12 06:40:52 +0500

    Update build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -15,11 +15,11 @@ build_kernel() {
 
     BUILD_VAR="-j$(nproc) -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
 
-    cat arch/arm64/configs/vendor/kona-sec-perf_defconfig arch/arm64/configs/vendor/samsung/$DEVICE.config \
-        arch/arm64/configs/ksu.config > arch/arm64/configs/temp_defconfig
+  cat arch/arm64/configs/vendor/kona-sec-perf_defconfig \
+    arch/arm64/configs/vendor/samsung/$DEVICE.config \
+    arch/arm64/configs/ksu.config > arch/arm64/configs/temp_defconfig
 
-CONFIG_LOCALVERSION="-AstroForge"
-    " >> arch/arm64/configs/temp_defconfig
+echo 'CONFIG_LOCALVERSION="-AstroForge"' >> arch/arm64/configs/temp_defconfig
 
     make $BUILD_VAR temp_defconfig
     rm arch/arm64/configs/temp_defconfig
